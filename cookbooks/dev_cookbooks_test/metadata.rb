3891df022673a4da151ebc5026a5cdf2d45a2d1b
Stas Turlo <stanislav.turlo@rightscale.com>
2013-08-12 17:03:37 +0300

    update metadata

--- cookbooks/dev_cookbooks_test/metadata.rb
+++ cookbooks/dev_cookbooks_test/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures dev_cookbooks_test'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe "dev_cookbooks_test::default", "test"
