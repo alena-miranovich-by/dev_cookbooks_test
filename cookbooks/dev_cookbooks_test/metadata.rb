1b85a6bd82f642bb1517ca0ad15e1d9adac234ce
Alena Mironovich <alena.mironovich@rightscale.com>
2013-08-20 18:12:20 +0300

    update recipe test

--- cookbooks/dev_cookbooks_test/metadata.rb
+++ cookbooks/dev_cookbooks_test/metadata.rb
@@ -7,3 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 recipe "dev_cookbooks_test::default", "test"
+recipe "dev_cookbooks_test::check_log_level", "test"
