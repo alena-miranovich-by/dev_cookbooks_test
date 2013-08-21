35dc9cd0854aeac59601f45a20f6a1dcf2da956f
miranovich <miranovich@miranovich-U1204.(none)>
2013-08-21 17:39:52 +0300

    update recipe

--- cookbooks/dev_cookbooks_test/recipes/check_log_level.rb
+++ cookbooks/dev_cookbooks_test/recipes/check_log_level.rb
@@ -12,26 +12,11 @@
 #agent_fail=0
 levels = ["info", "debug", "warn", "error", "fatal"]
 array = ["debug", "info","warn", "error", "fatal"]
-msg = ["test_message_with_info_cookbook_and_info_log_level",
-                "test_message_with_info_cookbook_and_warn_log_level",
-                "test_message_with_info_cookbook_and_error_log_level",
-                "test_message_with_info_cookbook_and_fatal_log_level",
-                "test_message_with_debug_cookbook_and_info_log_level",
-                "test_message_with_debug_cookbook_and_debug_log_level",
-                "test_message_with_debug_cookbook_and_warn_log-level",
-                "test_message_with_debug_cookbook_and_error_log_level",
-                "test_message_with_debug_cookbook_and_fatal_log_level",
-                "test_message_with_warn_cookbook_and_fatal_log_level",
-                "test_message_with_warn_cookbook_and_error_log_level",
-                "test_message_with_warn_cookbook_and_warn_log_level",
-                "test_message_with_fatal_cookbook_and_fatal_log_level",
-                "test_message_with_error_cookbook_and_fatal_log_level",
-                "test_message_with_error_cookbook_and_error_log_level"]
 
 levels.each do |lvl|
 
 	array.each do |val|
-		log "test_message_with_#{lvl}_cookbool_level_and_#{val}_log_level" do
+		log "test_message_with_#{lvl}_cookbook_level_and_#{val}_log_level" do
 			level val.to_sym
 		end
 	end
