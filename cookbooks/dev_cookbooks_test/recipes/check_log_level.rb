cca6c5aefa12c97c561485e44211f2a491dac177
miranovich <miranovich@miranovich-U1204.(none)>
2013-08-21 16:15:02 +0300

    update

--- cookbooks/dev_cookbooks_test/recipes/check_log_level.rb
+++ cookbooks/dev_cookbooks_test/recipes/check_log_level.rb
@@ -34,34 +34,30 @@ levels.each do |val|
 	system "rs_log_level -l #{val}"
 	system "rs_log_level | grep -i #{val}"
 	if $? == 0
-                puts "===PASS=== cookbook log level set successully"
-		system "echo '===PASS=== cookbook'"
+		system "echo '===PASS=== cookbook set'"
         else
-                puts "===FAIL=== cookbook log hasn't been set"
-		system "echo '==fail==cookbook'"
-                cookbook_fails += 1
-	        exit 101
+		system "echo '===FAIL=== cookbook not set'"
         end
 	#check logging process:
 
 	levels.each do |lvl|
-		puts "#{lvl}"
 		log "test_message_with_#{val}_cookbook_and_#{lvl}_log_level" do
 			level lvl.to_sym
 		end
 	end
-#	for path in '/var/log/messages' #'/var/log/syslog' 
-#		for msg in data_to_verify
-#			puts "#{msg}"
-#			system "cat #{path} | grep #{msg}"
-#			if $? == 0
-#				system "echo '==PASS=  exists'"
-#			else 
-#				system "echo '==FAIL== doesn't exist'"
-#			end
-#		end	
-#	end
 end
+	for path in '/var/log/messages' #'/var/log/syslog' 
+		for msg in data_to_verify
+#			puts "#{msg}"
+			system "cat #{path} | grep #{msg}"
+			if $? == 0
+				system "echo '==PASS=  exists'"
+			else 
+				system "echo '==FAIL== doesn't exist'"
+			end
+		end	
+	end
+#end
 
 #check agent log level 
 #levels.each do |val|
