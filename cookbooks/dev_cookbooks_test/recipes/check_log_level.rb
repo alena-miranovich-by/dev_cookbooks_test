293ab257ce904ee98251a82246ae36053a3f1e73
miranovich <miranovich@miranovich-U1204.(none)>
2013-08-21 15:32:11 +0300

    update

--- cookbooks/dev_cookbooks_test/recipes/check_log_level.rb
+++ cookbooks/dev_cookbooks_test/recipes/check_log_level.rb
@@ -12,21 +12,21 @@
 #agent_fail=0
 levels = ["info", "debug", "warn", "error", "fatal"]
 
-data_to_verify=["test_message to verify logging with info cookbook level and info log level",
-                "test_message to verify logging with info cookbook level and warn log level",
-                "test_message to verify logging with info cookbook level and error log level",
-                "test_message to verify logging with info cookbook level and fatal log level",
-                "test_message to verify logging with debug cookbook level and info log level",
-                "test_message to verify logging with debug cookbook level and debug log level",
-                "test_message to verify logging with debug cookbook level and warn log level",
-                "test_message to verify logging with debug cookbook level and error log level",
-                "test_message to verify logging with debug cookbook level and fatal log level",
-                "test_message to verify logging with warn cookbook level and fatal log level",
-                "test_message to verify logging with warn cookbook level and error log level",
-                "test_message to verify logging with warn cookbook level and warn log level",
-                "test_message to verify logging with fatal cookbook level and fatal log level",
-                "test_message to verify logging with error cookbook level and fatal log level",
-                "test_message to verify logging with error cookbook level and error log level"]
+data_to_verify=["test_message_with_info_cookbook_and_info_log_level",
+                "test_message_with_info_cookbook_and_warn_log_level",
+                "test_message_with_info_cookbook_and_error_log_level",
+                "test_message_with_info_cookbook_and_fatal_log_level",
+                "test_message_with_debug_cookbook_and_info_log_level",
+                "test_message_with_debug_cookbook_and_debug_log_level",
+                "test_message_with_debug_cookbook_and_warn_log-level",
+                "test_message_with_debug_cookbook_and_error_log_level",
+                "test_message_with_debug_cookbook_and_fatal_log_level",
+                "test_message_with_warn_cookbook_and_fatal_log_level",
+                "test_message_with_warn_cookbook_and_error_log_level",
+                "test_message_with_warn_cookbook_and_warn_log_level",
+                "test_message_with_fatal_cookbook_and_fatal_log_level",
+                "test_message_with_error_cookbook_and_fatal_log_level",
+                "test_message_with_error_cookbook_and_error_log_level"]
 
 
 #check cookbook log level and logging process
@@ -39,42 +39,42 @@ levels.each do |val|
         else
                 puts "===FAIL=== cookbook log hasn't been set"
 		system "echo '==fail==cookbook'"
-#               cookbook_fails += 1
-#	        exit 101
+                cookbook_fails += 1
+	        exit 101
         end
 	#check logging process:
+
 	levels.each do |lvl|
 		puts "#{lvl}"
 		log "test-message to verify logging with #{val} cookbook level and #{lvl} log level" do
 			level lvl.to_sym
 		end
 	end
-	for path in '/var/log/messages' #'/var/log/syslog' 
+	for path in 'test' #'/var/log/messages' #'/var/log/syslog' 
 		for msg in data_to_verify
+			puts "#{msg}"
 			system "cat #{path} | grep #{msg}"
 			if $? == 0
-				system "echo '==PASS= #{msg} exists'"
+				system "echo '==PASS=  exists'"
 			else 
-				system "echo '==FAIL== #{msg} doesn't exist'"
+				system "echo '==FAIL== doesn't exist'"
 			end
 		end	
 	end
 end
 
 #check agent log level 
-levels.each do |val|
-        system "rs_log_level --agent -l #{val}"
-        system "rs_log_level --agent | grep -i #{val}"
-        if $? == 0
-                puts "===PASS=== agent log level set successfully"
-        else
-                puts "===FAIL=== agent log level hasn't been set"
+#levels.each do |val|
+#        system "rs_log_level --agent -l #{val}"
+#        system "rs_log_level --agent | grep -i #{val}"
+#        if $? == 0
+#                puts "===PASS=== agent log level set successfully"
+#        else
+#                puts "===FAIL=== agent log level hasn't been set"
 #                agent_fail += 1
 #		exit 102
-        end
-
-
-end
+#        end
+#end
 #if cookbook_fail > 1 && agent_fail > 1
 #        puts "===PASS=== log_levels works correctly"
 #else
