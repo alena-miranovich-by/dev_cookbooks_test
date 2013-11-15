32c0a4be7ebdaab2158b85a6337296282940ac57
Alena Mironovich <alena.mironovich@rightscale.com>
2013-11-15 15:56:45 +0300

    rs_Tag

--- cookbooks/dev_cookbooks_test/recipes/default.rb
+++ cookbooks/dev_cookbooks_test/recipes/default.rb
@@ -1,23 +1,55 @@
 #
-# Cookbook Name:: dev_cookbooks_test
-# Recipe:: default
+# Cookbook Name:: rightlink_test
+# Recipe:: 
 #
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
 #
 
-#git "/tmp/right_link" do
-#  repository "https://github.com/rightscale/right_link.git"
-#  reference "master"
-#  action :sync
-#end
+# Check that chef is not using sandboxed ruby -- this is fixed by our custom ruby provider
+ruby_block "check rs_tag --query functionality" do
+  block do
+    tag1 = "user:name=tester"
+    tag2 = "test-tag with spaces"
+    tag3 = "test-tag_without_spaces"
+    unless node[:platform] == 'windows'
+    #linux
+    rightlink_version = `cat /etc/rightscale.d/rightscale-release`
+    if (rightlink_version.include? '5.9')
+      expected_output = ':tags=>["user:name=tester", "test-tag", "with", "spaces", "test-tag_without_spaces"'
+      result =`rs_tag --query '#{tag1} #{tag2} #{tag3}' -v`
+      puts result
 
-levels = Array.new
-levels = ["info","debug", "warn", "fatal", "error"]
+      if (result.include? expected_output)
+        chef::Log.info("5.9 cool")
+      else
+        Chef::Log.info("5.9 not cool")
+      end
+    elsif (rightlink_version.include? '6.0')
+      expected_output = ':tags=>["user:name=tester", "test-tag with spaces", "test-tag_without_spaces"'
+      result =`rs_tag --query #{tag1} '#{tag2}' #{tag3} -v`
+      puts result
 
-levels.each do |val|
-       	log "test message with level #{val}" do
-          	level val.to_sym
-       	 end
+      if (result.include? expected_output)
+        Chef::Log.info("6.0 cool")
+      else
+        Chef::Log.info("6.0 not cool")
+      end
+    end
+    else
+   #windows
+      expected_output = ':tags=>["user:name=tester", "test-tag", "with", "spaces", "test-tag_without_spaces"'
+      result =`rs_tag --query '#{tag1} #{tag2} #{tag3}' -v`
+      puts result
+
+      if (result.include? expected_output)
+        Chef::Log.info("windows 5.9 cool")
+      else
+        Chef::Log.info("windows 5.9 not cool")
+      end
+
+    end
+  end
 end
+
