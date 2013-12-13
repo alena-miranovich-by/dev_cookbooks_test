26bb6b17cc9d479972e546f8107e8be8d27e123d
Alena Miranovich <alena.mironovich@rightscale.com>
2013-12-13 12:20:57 +0300

    acu141267 - ssh key test temp

--- cookbooks/dev_cookbooks_test/metadata.rb
+++ cookbooks/dev_cookbooks_test/metadata.rb
@@ -4,7 +4,18 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures dev_cookbooks_test'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 recipe "dev_cookbooks_test::default", "test"
 recipe "dev_cookbooks_test::check_log_level", "test"
+recipe "rightlink_test::ssh_test_vscale", "Test to verify that the sshkey found in /var/spool/cloud/meta-data exists in /root/.ssh/authorizedkeys file"
+
+attribute "ssh_test/ssh_public_key_expected",
+  :display_name => "Input ssh_public_key expected",
+  :description => "True/False to execute test-case. Works only for Linux OS and vScale cloud.",
+  :required => "required",
+  :recipes => [ "rightlink_test::ssh_test_vscale" ],
+  :default => "false",
+  :type => "string"
+
+
---
+++ cookbooks/dev_cookbooks_test/recipes/ssh_key_test_vscale.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: rightlink_test
+# Recipe:: ssh_key_test_vscale
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+#
+# Verifies that sshkey found in /var/spool/cloud/meta-data exists in /root/.ssh/authorized_keys file
+
+metadata   = "/var/spool/cloud/meta-data.dict"
+auth_keys  = "/root/.ssh/authorized_keys"
+vscale_key = "VS_SSH_PUBLIC_KEY" 
+test_state = node[:ssh_test][:ssh_public_key_expected]
+
+ruby_block "Verify that ssh-key exists in authorized_keys" do
+  block do
+		# Check input: if it's false - to not run recipe at all"
+    if test_state == 'false'
+      Chef::Log.info "Will not run test-case, because of input is set to false"
+      exit 1
+    end
+
+    # Get the ssh-key defined in meta-data file
+    metadata_hash = Hash[File.read(metadata).split("\n").map{|i| i.split("=")}]
+
+    unless metadata_hash.nil?
+      if metadata_hash.has_key?(vscale_key)
+        ssh_key = metadata_hash[vscale_key]
+      else
+        Chef::Log.error "=== FAIL === There is no such key #{vscale_key} in #{metadata} file!"
+        exit 100
+      end
+    else
+      Chef::Log.error "=== FAIL === Cannot read #{metadata} file"
+      exit 101
+    end
+
+    Chef::Log.info "SSH_KEY for test purposes: #{ssh_key}"
+
+    # Check that founded ssh_key exists in authorized_keys file
+    if File.open(auth_keys, 'r').lines.any?{|line| line.include?("#{ssh_key}")}
+      Chef::Log.info "=== PASSED === vScale ssh-key exists in #{auth_keys} and verified with #{metadata}."
+    else 
+      Chef::Log.error "=== FAIL === vScale ssh-key doesn't exist in #{auth_keys} file!"
+      exit 102
+    end
+
+  end
+end
