7c2c9d40fa281a059fddaecc9c4f54b0cb97654e
Alena Miranovich <alena.mironovich@rightscale.com>
2013-12-13 14:57:55 +0300

    ssh key test

--- cookbooks/dev_cookbooks_test/recipes/ssh_key_test_vscale.rb
+++ cookbooks/dev_cookbooks_test/recipes/ssh_key_test_vscale.rb
@@ -16,25 +16,25 @@ test_state = node[:ssh_test][:ssh_public_key_expected]
 ruby_block "Verify that ssh-key exists in authorized_keys" do
   block do
 		# Check input: if it's false - to not run recipe at all"
-    if test_state == 'false'
-      Chef::Log.info "Will not run test-case, because of input is set to false"
-    else 
+#    if test_state == 'false'
+ #     Chef::Log.info "Will not run test-case, because of input is set to false"
+ #   else 
     # Get the ssh-key defined in meta-data file
-    metadata_hash = Hash[File.read(metadata).split("\n").map{|i| i.split("=")}]
+    metadata_hash = Hash[File.read(metadata).split("\n").map{|i| i.split("=")}] rescue nil
 
-    unless metadata_hash.nil?
+#    unless metadata_hash.nil?
       if metadata_hash.has_key?(vscale_key)
         ssh_key = metadata_hash[vscale_key]
       else
         Chef::Log.error "=== FAIL === There is no such key #{vscale_key} in #{metadata} file!"
         exit 100
       end
-    else
-      Chef::Log.error "=== FAIL === Cannot read #{metadata} file"
-      exit 101
-    end
+ #   else
+#      Chef::Log.error "=== FAIL === Cannot read #{metadata} file"
+ #     exit 101
+  #  end
 
-    Chef::Log.info "SSH_KEY for test purposes: #{ssh_key}"
+   # Chef::Log.info "SSH_KEY for test purposes: #{ssh_key}"
 
     # Check that founded ssh_key exists in authorized_keys file
     if File.open(auth_keys, 'r').lines.any?{|line| line.include?("#{ssh_key}")}
@@ -43,7 +43,8 @@ ruby_block "Verify that ssh-key exists in authorized_keys" do
       Chef::Log.error "=== FAIL === vScale ssh-key doesn't exist in #{auth_keys} file!"
       exit 102
     end
-    end
+    #end
+    not_if do test_state == 'false' end
 
   end
 end
