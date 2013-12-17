30bc482d3c3649b727527acd8b6744d4fc3defa9
Alena Mironovich <alena.mironovich@rightscale.com>
2013-12-17 16:36:56 +0300

    added check_ip_addresses recipe

--- cookbooks/dev_cookbooks_test/metadata.rb
+++ cookbooks/dev_cookbooks_test/metadata.rb
@@ -4,11 +4,12 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures dev_cookbooks_test'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 recipe "dev_cookbooks_test::default", "test"
 recipe "dev_cookbooks_test::check_log_level", "test"
 recipe "dev_cookbooks_test::ssh_key_test_vscale", "Test to verify that the sshkey found in /var/spool/cloud/meta-data exists in /root/.ssh/authorizedkeys file"
+recipe "dev_cookbooks_test::check_ip_addresses", "Test to verify is private IP private and Public IP - public"
 
 attribute "ssh_test/ssh_public_key_expected",
   :display_name => "Input ssh_public_key expected",
---
+++ cookbooks/dev_cookbooks_test/recipes/check_ip_addresses.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: rightlink_test
+# Recipe:: check_ip_addresses
+#
+# Copyright RightScale, Inc. All rights reserved. All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+#
+# Verify that private IP is private and public is public
+
+# Get ip addresses and verify
+ruby_block "verify private_ip is private and public_ip is public" do
+  block do
+    public_ipv4 = node[:cloud][:public_ipv4]
+    private_ipv4 = node[:cloud][:local_ipv4]
+    private_regexp = /\A(10\.|192\.168\.|172\.1[6789]\.|172\.2.\.|172\.3[01]\.)/
+
+    unless public_ipv4.nil? || private_ipv4.nil?
+      if public_ipv4 =~ private_regexp
+        Chef::Log.error "=== FAIL === Public IP #{public_ipv4} is private."
+        exit 101
+      end
+      unless private_ipv4 =~ private_regexp
+        Chef::Log.error "=== FAIL === Private IP #{private_ipv4} is not private."
+        exit 102
+      end
+    else
+      public_ipv4.nil? ? Chef::Log.info "Public IP is null" : Chef::Log.info "Private IP is null"
+    end
+  end
+end
+
