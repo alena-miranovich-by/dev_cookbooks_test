a4c227fc5e33c5981c2c29be105c01a160b127bf
Alena Miranovich <alena.mironovich@rightscale.com>
2013-12-19 19:13:59 +0300

    nat updates for win

--- cookbooks/dev_cookbooks_test/recipes/nat_test.rb
+++ cookbooks/dev_cookbooks_test/recipes/nat_test.rb
@@ -57,7 +57,8 @@ ruby_block "Verify NAT routes got setup correctly" do
         end
 
         route_regex = if node[:platform] == 'windows'
-                  network, mask = cidr_to_netmask.call(ENV["RS_NAT_RANGES"])
+                  network = route[0]
+                  mask = cidr_to_netmask.call(route[1])
                   /#{network}.*#{mask}.*#{@ip}/
                 else
                   /#{network}.*via.*#{@ip}/
