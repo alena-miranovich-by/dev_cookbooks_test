d9a01f8665ea83f8a10acca1c0ade1597d1b42ae
Alena Mironovich <alena.mironovich@rightscale.com>
2013-12-19 20:25:22 +0300

    nat test for Linux and Windows

--- cookbooks/dev_cookbooks_test/recipes/nat_test.rb
+++ cookbooks/dev_cookbooks_test/recipes/nat_test.rb
@@ -12,18 +12,18 @@
 #
 
 test_state = node[:nat_test][:nat_routes_expected]
-require 'ipaddr'
+#require 'ipaddr'
+
+if node[:platform] == 'windows'
+  load File.join(::Dir::COMMON_APPDATA, "Rightscale", "spool", "cloud", "meta-data.rb")
+else
+  require '/var/spool/cloud/meta-data'
+end
 
 ruby_block "Verify NAT routes got setup correctly" do
   block do
     routes_env = []
     missing_routes = []
-
-    if node[:platform] == 'windows'
-      load File.join(::Dir::COMMON_APPDATA, "Rightscale", "spool", "cloud", "meta-data.rb")
-    else 
-      require '/var/spool/cloud/meta-data'
-    end
 
     # Get RS_NAT_ADDRESS
     if ENV.key?("RS_NAT_ADDRESS")
@@ -31,71 +31,50 @@ ruby_block "Verify NAT routes got setup correctly" do
     else
       Process::abort("RS_NAT_ADDRESS is not defined in meta-data. Will not run test")
     end
-      # Get RS_NAT_RANGES from meta-data
-      if ENV.key?("RS_NAT_RANGES")
-        ranges = ENV["RS_NAT_RANGES"].split(",")
-       
-        # Parse to array [address, mask]
-        ranges.each do |route|
-          routes_env.push(route.split("/"))
-        end
+    # Get RS_NAT_RANGES from meta-data
+    if ENV.key?("RS_NAT_RANGES")
+      ranges = ENV["RS_NAT_RANGES"].split(",")
+     
+      # Parse to array [address, mask]
+      ranges.each do |route|
+        routes_env.push(route.split("/"))
+      end
 
-        cidr_to_netmask = Proc.new { |cidr| 
-          mask = IPAddr.new('255.255.255.255').mask(cidr).to_s
-          Chef::Log.info("#{mask} from proc")
-          mask
-        }
+      cidr_to_netmask = Proc.new { |cidr| 
+        mask = IPAddr.new('255.255.255.255').mask(cidr).to_s
+        Chef::Log.info("#{mask} from proc")
+        mask
+      }
 
-        unless node[:platform] == 'windows'
-          @routes_set = `ip route show`
+      unless node[:platform] == 'windows'
+        @routes_set = `ip route show`
+      else 
+        @routes_set = `route print`
+      end
+
+      routes_env.each do |route|
+        route_regex = if node[:platform] == 'windows'
+                network = route[0]
+                mask = cidr_to_netmask.call(route[1])
+                /#{network}.*#{mask}.*#{@ip}/
+              else
+                /#{network}.*via.*#{@ip}/
+              end
+        matchdata = @routes_set.match(route_regex)
+        if matchdata == nil
+           missing_routes.push(route)
         else 
-          @routes_set = `route print`
-        end
-
-        routes_env.each do |route|
-          route_regex = if node[:platform] == 'windows'
-                  network = route[0]
-                  mask = cidr_to_netmask.call(route[1])
-                  /#{network}.*#{mask}.*#{@ip}/
-                else
-                  /#{network}.*via.*#{@ip}/
-                end
-          matchdata = @routes_set.match(route_regex)
-          if matchdata == nil
-             missing_routes.push(route)
-          else 
-            Chef::Log.info("Route #{route[0]} matched")
-          end
-        end
-     end
-=begin
-          # Verify that every provided route got setup correctly on the instance
-          routes_env.each do |route|
-            # if mask is 32, it will not shown
-            if route[1] == "32"
-              missing_routes.push(route) unless routes_set.include? "#{route[0]} via #{@ip}"
-            else
-              missing_routes.push(route) unless routes_set.include? "#{route[0]}/#{route[1]} via #{@ip}"
-            end
-          end
-        else
-          # for windows
-          routes_set = `route PRINT -4`
-          
-          Chef::Log.info("#{routes_set}") 
-
-
+          Chef::Log.info("Route #{route[0]} matched")
         end
       end
-=end
-      unless missing_routes.empty?
-        Process::abort("=== FAIL === Routes have not been setup correctly. Missing routes: \n #{missing_routes.inspect}")
-      else
-        Chef::Log.info("=== PASS === NAT routes test completed successfully. Range from ENV has been setup correctly.")
-      end
-
     end
-    not_if do test_state == 'false' end
+    unless missing_routes.empty?
+      Process::abort("=== FAIL === Routes have not been setup correctly. Missing routes: \n #{missing_routes.inspect}")
+    else
+      Chef::Log.info("=== PASS === NAT routes test completed successfully. Range from ENV has been setup correctly.")
+    end
+
   end
-#end
+  not_if do test_state == 'false' end
+end
        
