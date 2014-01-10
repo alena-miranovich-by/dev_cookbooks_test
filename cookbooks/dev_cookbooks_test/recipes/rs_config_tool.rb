f645c339378f4db19a47a72a6431adf2678d994d
Alena Mironovich <alena.mironovich@rightscale.com>
2014-01-10 20:44:37 +0300

    updates

--- cookbooks/dev_cookbooks_test/recipes/rs_config_tool.rb
+++ cookbooks/dev_cookbooks_test/recipes/rs_config_tool.rb
@@ -54,11 +54,16 @@ end
 
 ruby_block "Verifies rs_config tool" do
   block do
-  class Chef::Recipe
-    include RightlinkTester::Utils
-  end
+    tag_exists? = Proc.new { |tag, uuid|
+      Chef::Log.info("Checking server collection for the #{tag}..")
+      tags_hash = node[:server_collection][uuid]
+      tags = tags_hash[tags_hash.keys[0]]
+      Chef::Log.info("Tags: #{tags.inspect}")
+      result = tags.select { |s| s == tag }
+    }
+
     # before changes:
-    if (tag_exists?(TAG, UUID).empty? and tag_exists?(TAG_DONE, UUID))
+    if (tag_exists?.call(TAG, UUID).empty? and tag_exists?.call(TAG_DONE, UUID))
       # =============== 1. Decommission timeout  =================
       # set decommission_timeout to very small value - 1 second
       decom_timeout = 1
@@ -101,7 +106,7 @@ ruby_block "Verifies rs_config tool" do
       # reboot the instance
       `rs_shutdown --reboot -i`
     # after reboot with applying changes
-    elsif (tag_exists?(TAG,UUID) and tag_exists?(TAG_DONE, UUID).empty?)
+    elsif (tag_exists?.call(TAG,UUID) and tag_exists?.call(TAG_DONE, UUID).empty?)
        # reset values to default
       `rs_config --set #{MANAGED_LOGIN_FEATURE} on`
       `rs_config --set #{MOTD_UPD_FEATURE} on`
