4000f1eab010d6b0d7de6b5140f6bffcc47a9923
Alena Mironovich <alena.mironovich@rightscale.com>
2014-02-13 09:13:46 +0300

    vscale updates

--- cookbooks/dev_cookbooks_test/recipes/ssh_key_test_vscale.rb
+++ cookbooks/dev_cookbooks_test/recipes/ssh_key_test_vscale.rb
@@ -13,23 +13,27 @@ test_state = node[:ssh_test][:ssh_public_key_expected]
 cloud = `cat /etc/rightscale.d/cloud`
 
 unless node[:platform] == 'windows' || cloud.strip! != "vscale"
-require '/var/spool/cloud/meta-data'
-ruby_block "Verify that ssh-key exists in authorized_keys" do
-  block do
-   # Chef::Log.info "SSH_KEY for test purposes: #{ssh_key}"
-   if  ENV.key?("VS_SSH_PUBLIC_KEY") 
-     ssh_key = ENV.key("VS_SSH_PUBLIC_KEY")
-     # Check that founded ssh_key exists in authorized_keys file
-     if File.open(auth_keys, 'r').lines.any?{|line| line.include?("#{ssh_key}")}
-       Chef::Log.info "=== PASSED === vScale ssh-key exists in #{auth_keys} and verified with #{metadata}."
-     else 
-       Chef::Log.error "=== FAIL === vScale ssh-key doesn't exist in #{auth_keys} file!"
-       exit 102
-     end
-   else 
-    exit 101  
-   end
-  end   
-  not_if do test_state == 'false' end
+  require '/var/spool/cloud/meta-data'
+  ruby_block "Verify that ssh-key exists in authorized_keys" do
+    block do
+      # Get vScale ssh-key value and verify it with authorized_keys file
+      if ENV.key?("VS_SSH_PUBLIC_KEY")
+        ssh_key = ENV["VS_SSH_PUBLIC_KEY"]
+
+        # Check that founded ssh_key exists in authorized_keys file
+        if File.open(auth_keys, 'r').lines.any?{|line| line.include?("#{ssh_key}")}
+          Chef::Log.info "=== PASSED === vScale ssh-key exists in #{auth_keys} and verified with meta-data."
+        else
+          Chef::Log.error "=== FAIL === vScale ssh-key doesn't exist in #{auth_keys} file!"
+          exit 102
+        end
+
+      else
+        Chef::Log.info "=== FAIL === VS_SSH_PUBLIC_KEY environment variable is not exists!"
+        exit 101
+      end
+    end
+    not_if do test_state == 'false' end
+  end
 end
-end
+
