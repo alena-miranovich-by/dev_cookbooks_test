16178357d8478fb049fc26c3fddb3aa9f08dc6fc
Alena Mironovich <alena.mironovich@rightscale.com>
2014-02-13 09:18:28 +0300

    vscale updates

--- cookbooks/dev_cookbooks_test/recipes/ssh_key_test_vscale.rb
+++ cookbooks/dev_cookbooks_test/recipes/ssh_key_test_vscale.rb
@@ -8,12 +8,12 @@
 #
 # Verifies that sshkey found in /var/spool/cloud/meta-data exists in /root/.ssh/authorized_keys file
 
-unless node[:platform] == 'windows' || cloud.strip! != "vscale"
-  require '/var/spool/cloud/meta-data'
-
   auth_keys  = "/root/.ssh/authorized_keys"
   test_state = node[:ssh_test][:ssh_public_key_expected]
   cloud = `cat /etc/rightscale.d/cloud`
+
+unless node[:platform] == 'windows' || cloud.strip! != "vscale"
+  require '/var/spool/cloud/meta-data'
 
   ruby_block "Verify that ssh-key exists in authorized_keys" do
     block do
