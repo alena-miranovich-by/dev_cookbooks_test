1720baafcc3223aa170fee6d1b571e86b57cfd97
Alena Miranovich <alena.mironovich@rightscale.com>
2014-03-17 12:54:16 +0300

    rs_config tool resets

--- cookbooks/dev_cookbooks_test/recipes/rs_config_tool.rb
+++ cookbooks/dev_cookbooks_test/recipes/rs_config_tool.rb
@@ -29,34 +29,18 @@ MOTD_UPD_FEATURE = "motd_update"
 REPO_FREEZE_FEATURE = "package_repositories_freeze"
 MOTD_SYSTEM_PATH = "/etc/motd"
 MOTD_MSG_ORIGINAL = "/tester/motd_original"
-#MOTD_MSG_TEST = "/tester/rs_config_motd"
-MOTD_MSG_TEST = "rs_config_motd"
+MOTD_MSG_TEST = "/tester/rs_config_motd"
 TIMESTAMP = "/tester/timestamp"
 TEST_MESSAGE = "rs_config test message"
 
-test_dir = if platform?('windows')
-  "C:\\tester"
-else 
-  "/tester"
-end
 
-directory "#{test_dir}" do
+directory "/tester" do
   action :create
 end
 
-template_path = ::File.join(test_dir, MOTD_MSG_TEST)
-template "#{template_path}" do
+template "#{MOTD_MSG_TEST}" do
   source "rs_config_motd.erb"
   action :create
-end
-
-ruby_block "test" do
-block do 
-timestamp = Time.now.to_i
-      `echo "#{timestamp}" > #{TIMESTAMP}`
-      `logger "#{TEST_MESSAGE} #{timestamp}"`
-
-end
 end
 
 ruby_block "Verifies rs_config tool" do
