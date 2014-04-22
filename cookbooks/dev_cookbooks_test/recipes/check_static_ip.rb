e17387e2c5db53e0d611bedbd7834a443fc04268
Alena Miranovich <alena.mironovich@rightscale.com>
2014-04-22 17:58:20 +0300

    typo

--- cookbooks/dev_cookbooks_test/recipes/check_static_ip.rb
+++ cookbooks/dev_cookbooks_test/recipes/check_static_ip.rb
@@ -4,8 +4,6 @@ class Chef::Resource::RubyBlock
   include RightlinkTester::Utils
 end
 
-
-
 network_adapter = node[:check_static_ip][:network_adapter]
 test_state = node[:check_static_ip][:test_enabled]
 
@@ -25,7 +23,6 @@ ruby_block "check static IP" do
       private_ipv4 = private_ipv4[0].to_s
     end
 
-
     public_ips = node[:cloud][:public_ips]  
     private_ips = node[:cloud][:private_ips]
     Chef::Log.info "private ips #{private_ips} and public ips #{public_ips}."
@@ -36,134 +33,85 @@ ruby_block "check static IP" do
       require '/var/spool/cloud/meta-data'
     end
 
-    for i in 0..1
-      key = "RS_IP"+i.to_s+"_ADDR"
-      if ENV.key?(key)
-        ip = ENV[key]
-        Chef::Log.info "ip from meta-data is #{ip}"
-        
-        case network_adapter
-        
-        when 'private'
-          if is_private? ip
-            if private_ipv4 == ip
-              Chef::Log.info "=== PASS === Current Private IP is the same as in meta-data."
-            elsif private_ips
-              if private_ips.include?(ip)
-                Chef::Log.info "=== WARN === Current Private IP is '#{private_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in private_ips array: #{private_ips}."
-              else 
-                raise "=== FAIL === Current Private IP ('#{private_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
-              end
-            end
-          else 
-            raise "=== FAIL === ip from meta-data is not private. Please make sure that input for test is provided correctly"
-          end
-        
-        when 'public'
-          unless is_private? ip
-            if public_ipv4 == ip
-              Chef::Log.info "=== PASS === Current Public IP is the same as in meta-data."
-            elsif public_ips
-              if public_ips.include?(ip)
-                Chef::Log.info "=== WARN === Current Public IP is '#{public_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in public_ips array: #{public_ips}."
-              else
-                raise "=== FAIL === Current Public IP ('#{public_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
-              end
-            end
-          else 
-            raise "=== FAIL === ip from meta-data is not public. Please make sure that input for test is provided correctly."
-          end
- 
-        when 'both'
-          if is_private? ip
-          # ip is private
-            if private_ipv4 == ip
-              Chef::Log.info "=== PASS === Current Private IP is the same as in meta-data."
-            elsif private_ips
-              if private_ips.include?(ip)
-                Chef::Log.info "=== WARN === Current Private IP is '#{private_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in private_ips array: #{private_ips}."
-              else
-                raise "=== FAIL === Current Private IP ('#{private_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
-              end
-            end
-
-          else
-          # ip is public
-            if public_ipv4 == ip
-              Chef::Log.info "=== PASS === Current Public IP is the same as in meta-data."
-            elsif public_ips
-              if public_ips.include?(ip)
-                Chef::Log.info "=== WARN === Current Public IP is '#{public_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in public_ips array: #{public_ips}."
-              else
-                raise "=== FAIL === Current Public IP ('#{public_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
-              end
-            end
-          end
-
-        end
-      end
-    end
-
-
-=begin
-    for i in 0..1
-      key = "RS_IP"+i.to_s+"_ADDR"
-      if ENV.key?(key)
-        #exists
-        ip = ENV[key]
-        Chef::Log.info "from metadata ip#{i} addr is #{ip}"
-
-        case network_adapter
-        when 'private'       
-Chef::Log.info "private block"
+    case network_adapter
+       
+    when 'private'
+      if ENV.key?("RS_IP0_ADDR")
+        ip = ENV["RS_IP0_ADDR"] 
+        if is_private? ip
           if private_ipv4 == ip
-            Chef::Log.info "=== PASS === IP from metadata is '#{ip}'. Current private IP is the same: '#{private_ipv4}'."
+            Chef::Log.info "=== PASS === Current Private IP is the same as in meta-data."
           elsif private_ips
             if private_ips.include?(ip)
-              Chef::Log.info "=== WARN === Current private IP is '#{private_ipv4}'. IP from metadata is '#{ip}' which is not set as current, but it exists in private_ips array: #{private_ips}."
+              Chef::Log.info "=== WARN === Current Private IP is '#{private_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in private_ips array: #{private_ips}."
             else 
-              raise "=== FAIL === Current private IP ('#{private_ipv4}') is not matched to static IP from metadata ('#{ip}'). Please check network settings."
+              raise "=== FAIL === Current Private IP ('#{private_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
             end
           end
-
-        when 'public' 
-Chef::Log.info "public block"
-          if public_ipv4 == ip
-            Chef::Log.info "=== PASS === IP from metadata is '#{ip}'. Current public IP is the same: '#{public_ipv4}'."
-          elsif public_ips
-            if public_ips.include?(ip)
-              Chef::Log.info "=== WARN === Current public IP is '#{public_ipv4}'. IP from metadata is '#{ip}' which is not set as current public IP, but it exists in public_ips array: #{public_ips}."
-            else   
-              raise "=== FAIL === Current public IP ('#{public_ipv4}') is not matched to static IP from metadata ('#{ip}'). Please check network settings."
-            end
-          end
-
-        when 'both' 
-Chef::Log.info "both block"
-          if public_ipv4 == ip
-            Chef::Log.info "=== PASS public === IP from metadata is '#{ip}'. Current public IP is the same: '#{public_ipv4}'."
-          elsif public_ips
-            if public_ips.include?(ip)
-              Chef::Log.info "=== WARN public === Current public IP is '#{public_ipv4}'. IP from metadata is '#{ip}' which is not set as current public IP, but it exists in public_ips array: #{public_ips}."
-            else 
-              # check if it is private
-              if private_ipv4 == ip
-                Chef::Log.info "=== PASS private === IP from metadata is '#{ip}'. Current private IP is the same: '#{private_ipv4}'."
-              elsif private_ips
-                if private_ips.include?(ip)
-                  Chef::Log.info "=== WARN private === Current private IP is '#{private_ipv4}'). IP from metadata is '#{ip}' which is not set as current private IP, but it exists in private_ips array: #{private_ips}."
-                else 
-                  raise "=== FAIL === Current public ('#{public_ipv4}') and private ('#{private_ipv4}') IPs are not matched to static IPs from metadata. Please check network settings."
-                end
-              end
-            end #
-          end #
+        else 
+          raise "=== FAIL === ip from meta-data is not private. Please make sure that input for test is provided correctly"
         end
+      else 
+        raise "=== FAIL === there is no RS_IP0_ADDR key in meta-data"
       end
-    end
-=end
-    Chef::Log.info "==================== check_static_ip recipe finished ===================="
+        
+     when 'public'
+       if ENV.key?("RS_IP0_ADDR")
+         ip = ENV["RS_IP0_ADDR"]
+         unless is_private? ip
+           if public_ipv4 == ip
+             Chef::Log.info "=== PASS === Current Public IP is the same as in meta-data."
+           elsif public_ips
+             if public_ips.include?(ip)
+               Chef::Log.info "=== WARN === Current Public IP is '#{public_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in public_ips array: #{public_ips}."
+             else
+               raise "=== FAIL === Current Public IP ('#{public_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
+             end
+           end
+         else 
+           raise "=== FAIL === ip from meta-data is not public. Please make sure that input for test is provided correctly."
+         end
+       else 
+         raise "=== FAIL === there is no RS_IP0_ADDR key in meta-data"
+       end
+ 
+     when 'both'
+       if ENV.key?("RS_IP0_ADDR") || ENV.key?("RS_IP1_ADDR")
+         for i in 0..1
+           key = "RS_IP"+i.to_s+"_ADDR"
+           ip = ENV[key]
+           if is_private? ip
+           # ip is private
+             if private_ipv4 == ip
+               Chef::Log.info "=== PASS === Current Private IP is the same as in meta-data."
+             elsif private_ips
+               if private_ips.include?(ip)
+                 Chef::Log.info "=== WARN === Current Private IP is '#{private_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in private_ips array: #{private_ips}."
+               else
+                 raise "=== FAIL === Current Private IP ('#{private_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
+               end
+             end
 
+           else
+           # ip is public
+             if public_ipv4 == ip
+               Chef::Log.info "=== PASS === Current Public IP is the same as in meta-data."
+             elsif public_ips
+               if public_ips.include?(ip)
+                 Chef::Log.info "=== WARN === Current Public IP is '#{public_ipv4}'. IP from metadata ('#{ip}') is not set as current, but it exists in public_ips array: #{public_ips}."
+               else
+                 raise "=== FAIL === Current Public IP ('#{public_ipv4}') is not the same as static IP from meta-data ('#{ip}')."
+               end
+             end
+           end
+         end
+       else 
+         raise "=== FAIL=== there are no RS_IP0_ADDR and RS_IP1_ADDR values in meta-data"
+       end
+    end
+
+    Chef::Log.info "==================== check_static_ip recipe finished ===================="
+ 
   end
   not_if { test_state == 'false' || node[:cloud][:provider] != 'vsphere' || get_rightlink_version.match('^6.0.[0-1]$') }
 end 
